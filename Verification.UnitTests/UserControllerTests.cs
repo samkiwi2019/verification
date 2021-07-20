6ca76b5324527c0111f6d53b28fe4fa6a6126561
Sam <sam.kiwi2019@gmail.com>
2021-07-20 21:59:45 +1200

    added unit testing code

--- Verification.UnitTests/UserControllerTests.cs
+++ Verification.UnitTests/UserControllerTests.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Moq;
 using Verification.Api.Controllers;
@@ -8,26 +9,18 @@ using Verification.Api.Data.IRepos;
 using Verification.Api.Dtos;
 using Verification.Api.Models;
 using Xunit;
-using Xunit.Abstractions;
-
 
 namespace Verification.UnitTests
 {
     public class UserControllerTests
     {
-        private readonly ITestOutputHelper _testOutputHelper;
         private readonly Mock<IUserRepo> _repositoryStub = new();
         private readonly Mock<ILogger<UserController>> _loggerStub = new();
         private readonly Mock<IMapper> _mapper = new();
         private readonly Random _rand = new();
 
-        public UserControllerTests(ITestOutputHelper testOutputHelper)
-        {
-            _testOutputHelper = testOutputHelper;
-        }
-
         [Fact]
-        public async Task CreateCustomerByEmail_WithUnitTests_ReturnsOk()
+        public async Task CreateCustomerByEmail_ReturnsOk()
         {
             // Arrange
             var expectedUserCreateDto = CreateRandomUser();
@@ -46,9 +39,24 @@ namespace Verification.UnitTests
             });
 
             // Assert
-            var createdItem = result;
-            _testOutputHelper.WriteLine(createdItem.ToString());
+            var okResult = result as OkObjectResult;
+            Assert.Equal(200, okResult.StatusCode);
 
+        }
+
+        [Fact]
+        public async Task GetVerificationCodeByEmail_ReturnsOk()
+        {
+            // Arrange
+            var controller = new UserController(_repositoryStub.Object, _mapper.Object, _loggerStub.Object);
+
+            // Act
+            var createEmail = CreateRandomEmail();
+            var result = await controller.GetVerificationCodeByEmail(createEmail);
+
+            // Assert
+            var okResult = result as OkObjectResult;
+            Assert.Equal(200, okResult.StatusCode);
         }
 
         private User CreateRandomUser()
@@ -58,6 +66,13 @@ namespace Verification.UnitTests
                 Id = _rand.Next(10000),
                 FullName = Guid.NewGuid().ToString(),
                 Phone = Guid.NewGuid().ToString(),
+                Email = Guid.NewGuid() + "@test.com"
+            };
+        }
+        private Parameters CreateRandomEmail()
+        {
+            return new()
+            {
                 Email = Guid.NewGuid() + "@test.com"
             };
         }
